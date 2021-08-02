c3109b84b35d84025edaab28a1340c1fd70f2fb7
jaredsimonelli <jaredsimonelli1234@gmail.com>
2021-08-02 11:58:36 -0400

    set up redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+// import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
+import { increment, decrement } from './actions';
+
 function App() {
-  return (
-      <Router>
-          <div className='container'>
-              
-          </div>
-      </Router>
-  );
+    const counter = useSelector(state => state.counter);
+    const isLogged = useSelector(state => state.isLogged);
+    const dispatch = useDispatch();
+
+    return (
+        <Router>
+            <div className='container'>
+                <h1>Counter {counter}</h1>
+                <button onClick={() => dispatch(increment(5))}>+</button>
+                <button onClick={() => dispatch(decrement())}>-</button>
+
+                {isLogged ? <h3>Need to log in to see this info</h3> : '' }
+                
+            </div>
+        </Router>
+    );
 }
 
 export default App;
