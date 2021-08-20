9ef0a0e8090ba16577ce1ecdf81f87d51ef4b726
jaredsimonelli <jaredsimonelli1234@gmail.com>
2021-08-19 23:27:05 -0400

    styling udpates for cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,7 +3,18 @@ import React from "react";
 function Cart() {
   return (
     <div>
-      <h3>My Cart</h3>
+      <h3 className="mt-5 ms-4 mb-3">My Cart</h3>
+      <div className="cart-container mx-4"></div>
+
+      <div className="mx-4 ">
+        <button
+          className="mt-3 checkout-btn btn btn-dark"
+          type="btn"
+          value="Checkout"
+        >
+          Checkout
+        </button>
+      </div>
     </div>
   );
 }
