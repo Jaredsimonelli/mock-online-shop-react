ec6f40aef63248acac295666674875e97d5efb9c
jaredsimonelli <jaredsimonelli1234@gmail.com>
2021-08-21 13:26:39 -0400

    ref redux cart in component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Cart() {
+  const store = useSelector((state) => state.store);
+  const cartItems = store.cart;
+  console.log(cartItems);
+
   return (
     <div>
       <h3 className="mt-5 ms-4 mb-3">My Cart</h3>
-      <div className="cart-container mx-4"></div>
+      <div className="cart-container mx-4">
+        {cartItems.map((item) => (
+          <p>{item.name}</p>
+        ))}
+      </div>
 
       <div className="mx-4 ">
         <button
