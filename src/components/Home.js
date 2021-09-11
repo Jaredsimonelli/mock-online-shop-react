703b485d7de083127eaea37b2a6bc34a1996faf6
jaredsimonelli <jaredsimonelli1234@gmail.com>
2021-09-10 23:56:02 -0400

    start on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { getIcon } from "../helpers/getIcon";
 
 function Home() {
   return (
     <div>
       <h1 className="text-center mt-3">MOCK SHOP</h1>
       <h5 className="text-center mt-3">Welcome to the mock react shop!</h5>
+      {/* Placeholder for carousel and other home page relate stuff */}
     </div>
   );
 }
