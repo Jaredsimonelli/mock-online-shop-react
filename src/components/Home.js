2ee8c7bae44f5537a6ce3fe03a76b3b508bc091c
jaredsimonelli <jaredsimonelli1234@gmail.com>
2021-09-12 17:36:01 -0400

    carousel home begin

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,76 @@
 import React from "react";
 import { getIcon } from "../helpers/getIcon";
+import "jquery/src/jquery";
 
 function Home() {
   return (
     <div>
       <h1 className="text-center mt-3">MOCK SHOP</h1>
       <h5 className="text-center mt-3">Welcome to the mock react shop!</h5>
-      {/* Placeholder for carousel and other home page relate stuff */}
+
+      <div
+        id="carouselExampleIndicators"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="..." className="d-block w-100" alt="..."></img>
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..."></img>
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..."></img>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
   );
 }
